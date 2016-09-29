a88774704bd2cd33773a7a923d3200aaa22b97ae
UdayKiranDora <uskc2@mail.umkc.edu>
2016-09-28 23:14:12 -0500

    commiting lab5

---
+++ Assignment 5/Source/js/app.js
@@ -0,0 +1,49 @@
+
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+    .state('register',{
+      url:'/register',
+        templateUrl: 'templates/register.html'
+
+  })
+    .state('hh', {
+      url: '/hh',
+      templateUrl: 'templates/hh.html'
+    })
+    .state('mashup',{
+      url:'/mashup',
+      templateUrl: 'templates/mashup.html'
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
